61f4a22d15ad6aae937938333832c8df2361022d
shin.jewoo <shinjw0926@naver.com>
2023-04-12 22:25:39 +0900

    API 개발 고급 - 지연 로딩과 조회 성능 최적화 [간단한 주문 조회 V1: 엔티티를 직접 노출]

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne (ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * 이렇게 쓰지 말 것 !!
+     * 엔티티를 API응답으로 반환하지 말 것.
+     */
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy때문에 프록시객체 강제 초기화
+            order.getDelivery().getStatus(); //Lazy때문에 프록시객체 강제 초기화
+        }
+        return all;
+    }
+}
