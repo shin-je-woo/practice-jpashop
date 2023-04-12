15f2a1bfd00f0f1100d4978abaf7f850da745c7d
shin.jewoo <shinjw0926@naver.com>
2023-04-12 23:03:54 +0900

    API 개발 고급 - 지연 로딩과 조회 성능 최적화 [간단한 주문 조회 V2: 엔티티를 DTO로 변환]

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,19 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * xToOne (ManyToOne, OneToOne)
@@ -33,5 +39,33 @@ public class OrderSimpleApiController {
             order.getDelivery().getStatus(); //Lazy때문에 프록시객체 강제 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDTO> ordersV2() {
+        // N+1 문제 발생
+        // Order 2개 (1번)
+        // 회원, 주문(Lazy초기화) 2번 -> 4번
+        // 총 5번의 쿼리 발생
+        return orderRepository.findAllByString(new OrderSearch()).stream()
+                .map(SimpleOrderDTO::new)
+                .collect(toList());
+    }
+
+    @Data
+    static class SimpleOrderDTO {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDTO(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //Lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //Lazy 초기화
+        }
     }
 }
