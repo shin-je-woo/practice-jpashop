b85b6568c9c22b9d0237964079e0845fcf5fe39f
shin.jewoo <shinjw0926@naver.com>
2023-04-15 22:19:00 +0900

    API 개발 고급 - 컬렉션 조회 최적화 [주문 조회 V4: JPA에서 DTO 직접 조회]

---
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.repository.order.query;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderQueryRepository {
+
+    private final EntityManager em;
+
+
+    public List<OrderQueryDTO> findOrderQueryDtos() {
+        List<OrderQueryDTO> result = findOrders(); //query 1번 -> N개
+        result.forEach(o -> {
+            List<OrderItemQueryDTO> orderItems = findOrderItems(o.getOrderId()); //query N번
+            o.setOrderItems(orderItems);
+        });
+        return result;
+    }
+
+    private List<OrderItemQueryDTO> findOrderItems(Long orderId) {
+        return em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDTO(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                        " from OrderItem oi" +
+                        " join oi.item i" +
+                        " where oi.order.id = :orderId", OrderItemQueryDTO.class)
+                .setParameter("orderId", orderId)
+                .getResultList();
+    }
+
+    private List<OrderQueryDTO> findOrders() {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderQueryDTO(o.id, m.name, o.orderDate, o.status, d.address)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d", OrderQueryDTO.class)
+                .getResultList();
+    }
+}
